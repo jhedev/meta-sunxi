6f2e1a6c54c44b5d94945e703593d0ca57f88257
Joel Hermanns <joel.hermanns@gmail.com>
2016-11-10 16:44:33 +0000

    Fix boot.cmd
    Conflicts:
    	recipes-bsp/u-boot/files/boot.cmd

--- recipes-bsp/u-boot/files/boot.cmd
+++ recipes-bsp/u-boot/files/boot.cmd
@@ -1,4 +1,4 @@
 setenv bootargs console=${console} console=tty1 root=/dev/mmcblk0p${mender_boot_part} rootwait panic=10 ${extra}
-load ${mender_uboot_boot} ${fdt_addr_r} ${fdtfile} 
-load ${mender_uboot_boot} ${kernel_addr_r} uImage || load ${mender_uboot_root} ${kernel_addr_r} boot/uImage
+load ${mender_uboot_root} ${fdt_addr_r} boot/${fdtfile}
+load ${mender_uboot_root} ${kernel_addr_r} boot/uImage
 bootm ${kernel_addr_r} - ${fdt_addr_r}
