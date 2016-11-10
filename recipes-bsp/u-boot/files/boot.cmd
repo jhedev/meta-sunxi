4a68708de77e3a01226152ddfb088b401019e1be
Joel Hermanns <joel.hermanns@gmail.com>
2016-11-10 16:44:33 +0000

    Update boot.cmd file

--- recipes-bsp/u-boot/files/boot.cmd
+++ recipes-bsp/u-boot/files/boot.cmd
@@ -1,4 +1,4 @@
-setenv bootargs console=${console} console=tty1 root=/dev/mmcblk0p2 rootwait panic=10 ${extra}
-load mmc 0:1 ${fdt_addr_r} ${fdtfile} || load mmc 0:1 ${fdt_addr_r} boot/${fdtfile}
-load mmc 0:1 ${kernel_addr_r} zImage || load mmc 0:1 ${kernel_addr_r} boot/zImage || load mmc 0:1 ${kernel_addr_r} uImage || load mmc 0:1 ${kernel_addr_r} boot/uImage
-bootz ${kernel_addr_r} - ${fdt_addr_r} || bootm ${kernel_addr_r} - ${fdt_addr_r}
+setenv bootargs console=${console} console=tty1 root=/dev/mmcblk0p${mender_boot_part} rootwait panic=10 ${extra}
+load ${mender_uboot_root} ${fdt_addr_r} boot/${fdtfile}
+load ${mender_uboot_root} ${kernel_addr_r} boot/uImage
+bootm ${kernel_addr_r} - ${fdt_addr_r}
